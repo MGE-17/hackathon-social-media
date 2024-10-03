6a4488df3bfe6c1f4e07a85c6db5a5264a6b789f
Miguel Gomez <miguelgomeze17@gmail.com>
2024-10-03 14:39:15 -0500

    connect api

--- social-media-client/src/App.jsx
+++ social-media-client/src/App.jsx
@@ -6,7 +6,7 @@ import Image from "./components/image/image";
 function App() {
   return (
     <section className="social">
-      <h1 className="social__title">Social Media Site</h1>
+      <h1 className="social__title">ChatterBox</h1>
       <div className="social__image">
         <Image />
         <Comments />
